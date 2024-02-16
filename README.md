38f28e3fe4211dbef9327c6494bce18eedf5c371
IreneQuero <queroirene@gmail.com>
2024-02-16 18:03:18 +0100

    Proyecto IP

---
+++ README.md
@@ -0,0 +1,7 @@
+# Geolocation-IP
+
+Proyecto de geolocalización de IP creado con JavaScript, consumiendo una API donde te devuelve los datos de la IP introducida. Podrás consultar país, idioma, moneda y más información.
+
+Para la producción de este proyecto, he utilizado las siguientes herramientas: "Codi.link" como editor de código, "RapidAPI" para el consumo de la API, el Framework "Pico.css" para los estilos (adaptándolo a las necesidades del proyecto) y la librería "Particles.js" para agregar un fondo dinámico.
+
+![Imagen de ejemplo](ejemploip.png)
---
+++ script.js
@@ -0,0 +1,152 @@
+const OPTIONS = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'API Key',
+    'X-RapidAPI-Host': 'user-ip-data-rest-api.p.rapidapi.com'
+  }
+};
+
+const fetchInfo = ip => {
+  const url = `https://user-ip-data-rest-api.p.rapidapi.com/?ip=${ip}`;
+  return fetch(url, OPTIONS)
+    .then(res => res.json())
+    .catch(err => console.error(err))
+}
+
+
+const $form = document.querySelector('#form')
+const $input = document.querySelector('#input')
+const $submit = document.querySelector('#submit')
+const $results = document.querySelector('#results')
+
+$form.addEventListener('submit', async (event) => {
+  event.preventDefault()
+  const {value} = $input
+  if (!value) return
+
+  $submit.setAttribute('disabled', '')
+  $submit.setAttribute('aria-busy', 'true')
+
+  const ipInfo = await fetchInfo(value)
+
+  if (ipInfo){
+    $results.innerHTML = JSON.stringify(ipInfo, null, 2)
+  }
+
+  $submit.removeAttribute('disabled')
+  $submit.removeAttribute('aria-busy')
+})
+
+particlesJS(
+  {
+    "particles": {
+      "number": {
+        "value": 6,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#1b1e34"
+      },
+      "shape": {
+        "type": "polygon",
+        "stroke": {
+          "width": 0,
+          "color": "#000"
+        },
+        "polygon": {
+          "nb_sides": 6
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 160,
+        "random": false,
+        "anim": {
+          "enable": true,
+          "speed": 10,
+          "size_min": 40,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 200,
+        "color": "#ffffff",
+        "opacity": 1,
+        "width": 2
+      },
+      "move": {
+        "enable": true,
+        "speed": 8,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+)
+
